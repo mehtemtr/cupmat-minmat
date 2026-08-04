9ec4df82c7c9b9965c1447a2799cc85bb2346088
mahtemtr <mehtem2016@gmail.com>
2026-08-04 23:01:06 +0300

    chore: migrate to opennextjs-cloudflare

--- app/api/og/minmat/route.tsx
+++ app/api/og/minmat/route.tsx
@@ -1,7 +1,7 @@
 import { ImageResponse } from "next/og";
 import { NextRequest } from "next/server";
 
-export const runtime = "edge";
+export const dynamic = "force-dynamic";
 
 const translations = {
   tr: {
